e87ea354a98e467b9a42ec0cebfee330c05edb0d
orukahairuka <sakuraierika03301109@gmail.com>
2023-09-01 20:06:50 +0900

    if文を復習した！

---
+++ 1.dart
@@ -0,0 +1,12 @@
+void main() {
+  int mathScore = 100; //数学の点数
+  if (mathScore >= 80) {
+    print('優');
+  } else if (mathScore >= 60) {
+    print('良');
+  } else if (mathScore >= 30) {
+    print('可');
+  } else {
+    print('不可');
+  }
+}
