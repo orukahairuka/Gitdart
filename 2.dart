68230b1ab4405681a53b6c8f9e7abdaf49366ada
orukahairuka <sakuraierika03301109@gmail.com>
2023-09-01 21:10:25 +0900

    こんど立つ初心者編が出るらしい

--- 2.dart
+++ 2.dart
@@ -1,1 +1,12 @@
+void main() {
+  bool isPremiumMember = true; //プレミアム会員：true
+  bool isExpired = false; //期限ぎれ：false
+  bool isLiveInTokyo = true; //東京在住：true
+  bool isLiveInOsaka = false; //大阪在住：false
 
+  if (isPremiumMember && !isExpired && (isLiveInTokyo || isLiveInOsaka)) {
+    print('キャンペーン対象');
+  } else {
+    print('キャンペーン対象ではない');
+  }
+}
